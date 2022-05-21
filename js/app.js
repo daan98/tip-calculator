72ce60cb471fe6d95969847723dcf746c9103bda
daan98 <daan98.da@gmail.com>
2022-05-21 01:53:02 -0500

    showing intake sumamry when client select dish(es) and delete information on click 'Delete dish' button

--- js/app.js
+++ js/app.js
@@ -59,13 +59,13 @@ function getDishes(){
 function showDishInfo(dishes){
     const dishInfo = document.querySelector('.dish-container');
 
-    dishes.forEach(dish => {
+    dishes.forEach(dish => { // DISPLAYING DISHES INFORMATION
         const row = document.createElement('div');
         row.classList.add('row', 'py-3', 'border-top');
         
         const name = document.createElement('div');
         name.classList.add('col-md-4');
-        name.textContent = dish.nombre;
+        name.textContent = dish.name;
         row.appendChild(name);
         
         const price = document.createElement('div');
@@ -96,12 +96,137 @@ function showDishInfo(dishes){
 
 function addDishes(plate) {
     const { order } = client;
-    console.log(order);
 
     if(plate.quantity > 0){
         client.order = [...order, plate];
-        console.log(order.some(element => element.id === plate.id));
+
+        if(order.some(element => element.id === plate.id)){ // IF THE PLATE ALREADY EXISTS WE JUST UPDATE ITS QUANTITY
+
+            const orderUpdated = order.map( articleUpdated => {
+                if(articleUpdated.id === plate.id) articleUpdated.quantity = plate.quantity
+
+                return articleUpdated;
+            });
+
+            client.order = [...orderUpdated]
+
+        } else client.order = [...order, plate]
     }else{
-        console.log(`plate ${plate.nombre} quantity: ${plate.quantity}`);
+        const result = order.filter(singleOrder => singleOrder.id !== plate.id);
+        client.order = [...result];
     }
+
+    // UPDATING SUMMARY
+    const content = document.querySelector('#summary .contenido');
+    cleanSummary(content);
+    showSummary(content);
+}
+
+function cleanSummary(content){     while(content.firstChild) content.removeChild(content.firstChild);     }
+
+function showSummary(content){
+    const summary = document.createElement('div');
+    summary.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+
+    // HEADING
+    const heading = document.createElement('h3');
+    heading.classList.add('my-4', 'text-center');
+    heading.textContent = 'SUMMARY HEADING HERE';
+    summary.appendChild(heading);
+
+    // TABLE
+    const table = document.createElement('p');
+    table.classList.add('fw-bold');
+    table.textContent = 'Table: ';
+    summary.appendChild(table);
+
+    const tableSpan = document.createElement('span');
+    tableSpan.classList.add('fw-normal');
+    tableSpan.textContent = client.table;
+    table.appendChild(tableSpan);
+
+    // HOUR
+    const hour = document.createElement('p');
+    hour.classList.add('fw-bold');
+    hour.textContent = 'Hour: ';
+    summary.appendChild(hour);
+
+    const hourSpan = document.createElement('span');
+    hourSpan.classList.add('fw-normal');
+    hourSpan.textContent = client.hour;
+    hour.appendChild(hourSpan);
+
+    // DISH INFORMATION
+    const group = document.createElement('ul');
+    group.classList.add('list-group');
+
+    client.order.forEach(dish =>{
+        const {id, name, price, quantity}  = dish;
+        
+        const list = document.createElement('li');
+        list.classList.add('list-group-item');
+        
+        // NAME
+        const nameEl = document.createElement('h4');
+        nameEl.classList.add('my-4');
+        nameEl.textContent = name;
+        list.appendChild(nameEl);
+        
+        // QUANTITY
+        const quantityEl = document.createElement('p');
+        quantityEl.classList.add('fw-bold');
+        quantityEl.textContent = 'Quantity: ';
+        list.appendChild(quantityEl);
+
+        const quantityValue = document.createElement('span');
+        quantityValue.classList.add('fw-normal');
+        quantityValue.textContent = quantity;
+        quantityEl.appendChild(quantityValue);
+
+        // PRICE
+        const priceEl = document.createElement('p');
+        priceEl.classList.add('fw-bold');
+        priceEl.textContent = 'Price: ';
+        list.appendChild(priceEl);
+
+        const priceValue = document.createElement('span');
+        priceValue.classList.add('fw-normal');
+        priceValue.textContent = price;
+        priceEl.appendChild(priceValue);
+
+        // SUBTOTAL
+        const subtotalEl = document.createElement('p');
+        subtotalEl.classList.add('fw-bold');
+        subtotalEl.textContent = 'Subtotal: ';
+        list.appendChild(subtotalEl);
+
+        const subtotalValue = document.createElement('span');
+        subtotalValue.classList.add('fw-normal');
+        subtotalValue.textContent = subtotalCalculation(quantity, price);
+        subtotalEl.appendChild(subtotalValue);
+
+        // DELETE BUTTON
+        const btnDelete =  document.createElement('button');
+        btnDelete.classList.add('btn', 'btn-danger');
+        btnDelete.textContent = 'Delete dish';
+        btnDelete.onclick = () => {     deleteDish(id)     };
+        list.appendChild(btnDelete);
+
+        group.appendChild(list);
+        summary.appendChild(group);
+    });
+
+    content.appendChild(summary);
+}
+
+function subtotalCalculation(a, b){     return a * b;     }
+
+function deleteDish(id){
+    const { order } = client;
+    const result = order.filter(orderUpdated => orderUpdated.id !== id);
+    client.order = [...result]
+
+    const content = document.querySelector('#summary .contenido');
+    cleanSummary(content);
+    showSummary(content);
 }
